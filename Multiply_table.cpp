25f0b60d1183281450ef36df84924c7d6cafa0a3
prashant-24 <47084260+prashant-24@users.noreply.github.com>
2024-10-09 12:24:57 +0530

    Create Multiply_table.cpp

---
+++ Multiply_table.cpp
@@ -0,0 +1,14 @@
+// C++ program to print table
+// of a number
+#include <iostream>
+using namespace std;
+
+// Driver code
+int main()
+{
+	// Change here to change output
+	int n = 5;
+	for (int i = 1; i <= 10; ++i)
+		cout << n << " * " << i << " = " << n * i << endl;
+	return 0;
+}
